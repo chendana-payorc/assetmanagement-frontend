bb2344dcfe6c3fd47b04db8edef743f0e4e4fbd9
chandana priya <chandanapriyakattamanchi@gmail.com>
2025-11-07 19:52:51 +0530

    changed the asset controller

---
+++ frontend/app/Controllers/AssetController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class AssetController extends BaseController
+{
+    public function index(): string
+    {
+        return view('dashboard');
+    }
+
+    
+}
